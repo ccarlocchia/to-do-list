6720d702e2a84f945cd713dfe5695dd240c6f517
Camila B. Carlocchia <ccarlocchia@gmail.com>
2024-03-20 21:41:03 -0300

    try to add to gh pages

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,6 +3,10 @@
 	<meta name="description" content="About this app" />
 </svelte:head>
 
+<script>
+	import { base } from "$app/paths";
+</script>
+
 <div class="text-column">
 	<h1 class="about-me-title">About this project...</h1>
 
@@ -18,7 +22,7 @@
 		Whenever I got stuck, I was thankfull to find an answer to some of the issues I faced looking at a similar project that was made in a <a href="https://joyofcode.xyz/svelte-todo-app" target="_blank">Tutorial from JoyOfCode</a>.
 	</p>
 	<p>
-		A <a href="/sverdle">wordle</a> come included with the project when I started, and as a wordle lover I can't simply remove it, so feel free to play with it.
+		A <a href="{base}/sverdle">wordle</a> come included with the project when I started, and as a wordle lover I can't simply remove it, so feel free to play with it.
 	</p>
 </div>
 
